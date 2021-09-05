38a3305fa0a2613430ecdda4f312a9a536409e2c
Dipankar Ranjan Baisya <dipankar0705018@gmail.com>
2021-09-05 12:17:16 -0700

    Time: 200 ms (52.88%), Space: 17.5 MB (96.45%) - LeetHub

--- kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -6,21 +6,17 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        minheap_queue = []
-        
+        min_heap = []
         rows = len(matrix)
         cols = len(matrix[0])
-        
-        x = min(k,rows)
+        x = min(k, rows)
         
         for i in range(0,x):
-            minheap_queue.append((matrix[i][0],i,0))
-        hq.heapify(minheap_queue)
-        
+            min_heap.append((matrix[i][0],i,0))
+        hq.heapify(min_heap)
         while k:
-            num, row, col = hq.heappop(minheap_queue)
-            if col<cols-1: 
-                hq.heappush(minheap_queue,(matrix[row][col+1],row,col+1))
-            k-=1
-            
+            num,row,col = hq.heappop(min_heap)
+            if col<cols-1:
+                hq.heappush(min_heap,(matrix[row][col+1],row,col+1))
+            k -= 1
         return num
