445a1061844efa24e308a60cebc5c8dfeb2c00fd
Dipankar Ranjan Baisya <dipankar0705018@gmail.com>
2021-09-10 21:02:50 -0700

    Time: 38 ms (15.80%), Space: 13.7 MB (68.78%) - LeetHub

---
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,52 @@
+class Solution(object):
+    def getpvt(self, nums):
+        low = 0
+        high = len(nums) -1  
+        while(high >= low):
+            mid = (low+high)//2
+            #print(mid)
+            if nums[mid]>nums[mid+1]:
+                return mid+1
+            if nums[mid]<nums[mid-1]:
+                return mid
+            if nums[mid]>nums[low]:
+                low = mid+1
+            else:
+                high = mid - 1
+
+    def binsearch(self,nums, target):
+        low = 0
+        high = len(nums) -1
+        while(high >= low):
+                mid = (low+high)//2
+                if nums[mid] == target:
+                    return mid
+                if target>nums[mid]:
+                    low = mid+1
+                else:
+                    high = mid - 1
+        return -1
+    
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if len(nums)==1:
+            return 0 if nums[0]==target else -1
+        if nums[0]<nums[-1]:
+            return  self.binsearch(nums, target)
+        else:
+            pvt_index = self.getpvt(nums)
+            print(pvt_index)
+            if target == nums[pvt_index]:
+                return pvt_index
+            if target< nums[0]:
+                idx = self.binsearch(nums[pvt_index+1:],target)
+                if idx >=0:
+                    return pvt_index + 1+self.binsearch(nums[pvt_index+1:],target)
+                else:
+                    return -1
+            else:
+                return self.binsearch(nums[0:pvt_index], target)
