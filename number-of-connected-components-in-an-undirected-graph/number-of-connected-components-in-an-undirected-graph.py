765214c32616f8d67541be6349445c0a179716c2
Dipankar Ranjan Baisya <dipankar0705018@gmail.com>
2021-10-09 16:43:18 -0700

    Time: 92 ms (51.99%), Space: 14.5 MB (98.48%) - LeetHub

---
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.py
@@ -0,0 +1,49 @@
+class Solution(object):
+    def countComponents(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        # initiate the class of UnionFind
+        uf = UnionFind(n)
+        for e in edges:
+            uf.union(e[0],e[1])
+        
+        
+        # number of unique entries in root array is the number of connected components
+        count = 0
+        for i in range(0,n):
+            if i == uf.root[i]:
+                count+=1
+        
+        return count
+        
+# UnionFind class
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+        
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    # The union function with union by rank
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
