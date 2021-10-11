50659eb6f07af7fb989bbae9fc4d5003e6ea6f5e
Dipankar Ranjan Baisya <dipankar0705018@gmail.com>
2021-10-11 11:20:46 -0700

    Time: 114 ms (21.56%), Space: 14.7 MB (89.16%) - LeetHub

---
+++ graph-valid-tree/graph-valid-tree.py
@@ -0,0 +1,49 @@
+class Solution(object):
+    def validTree(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: bool
+        """
+        if len(edges) != (n-1):
+            return False
+        uf = UnionFind(n)
+        for e in edges:
+            uf.union(e[0],e[1])
+        count = 0
+        
+        for i in range(0,n):
+            if i == uf.root[i]:
+                count+=1
+        return True if count==1 else False
+
+# UnionFind class
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+        
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    # The union function with union by rank
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+
+        
